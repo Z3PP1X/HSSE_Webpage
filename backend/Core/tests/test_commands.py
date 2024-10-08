15dfd3ba0a137ee2a72d5883c7b6e7198fb65a54
Patrick <84373546+Z3PP1X@users.noreply.github.com>
2024-10-08 13:24:32 +0200

    Fixed Linting Error

--- backend/Core/tests/test_commands.py
+++ backend/Core/tests/test_commands.py
@@ -8,7 +8,7 @@ from psycopg2 import OperationalError as Psycopg2Error
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
-from Core.management.commands.wait_for_db import Command
+
 
 @patch('Core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
