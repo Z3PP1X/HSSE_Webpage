2c4b72c54a2966036743b8003a871de3db6f54f7
Patrick <84373546+Z3PP1X@users.noreply.github.com>
2024-10-08 13:21:41 +0200

    Fixed import error

--- backend/Core/tests/test_commands.py
+++ backend/Core/tests/test_commands.py
@@ -8,9 +8,9 @@ from psycopg2 import OperationalError as Psycopg2Error
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
+from Core.management.commands.wait_for_db import Command
 
-
-@patch('core.management.commands.wait_for_db.Command.check')
+@patch('Core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Test commands."""
 
