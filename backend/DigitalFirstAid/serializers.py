51110c562f2aff2ec70e36ead6ab087c27c38cb1
Z3PP1X <p.fuchs@hm.edu>
2025-04-17 09:33:55 +0200

    ---

--- backend/DigitalFirstAid/serializers.py
+++ backend/DigitalFirstAid/serializers.py
@@ -6,6 +6,8 @@ from rest_framework import serializers
 
 from .hsseModules.FirstAidRecord import FirstAidRecord
 
+from JsonSchemaForms.model_schema_builder import ModelFormSchemaBuilder
+
 
 class FirstAidRecordSerializer(serializers.ModelSerializer):
     """Serializer for the first aid record object."""
@@ -25,31 +27,37 @@ class FirstAidRecordMetadataSerializer(serializers.BaseSerializer):
     """Serializer for the Models Metadata"""
 
     def get_model_metadata(model, meta_whitelist=[]):
-        field_names = model._meta.fields
-        return_data = {}
-        super_attributes = ("id", "sys_id", "created_by",
-                            "updated_by", "updated_on",
-                            "created_on", "Active")
-
-        for field in field_names:
-            if field.name in super_attributes:
-                continue
-
-            field_name = field.name
-            field_meta = field.__dict__
-            return_meta = {}
-            return_meta['key'] = field.name
-
-            for meta_name in field_meta:
-                if meta_name in meta_whitelist:
-                    return_meta[meta_name] = field_meta[meta_name]
-                    return_meta['field_type'] = field.get_internal_type()
-
-                if field.choices:
-                    return_meta['choices'] = [
-                        {'value': choice[0], 'display_name': choice[1]}
-                        for choice in field.choices]
-
-            return_data[field_name] = return_meta
-
-        return return_data
+        # Create a new ModelFormSchemaBuilder instance
+        builder = ModelFormSchemaBuilder(
+            model_class=model,
+            form_id="first_aid_record_form",
+            form_title="First Aid Record Form"
+        )
+        
+        # Configure with dummy categories (to be replaced later)
+        config = {
+            'categories': [
+                {
+                    'key': 'incident_details',
+                    'title': 'Incident Details',
+                    'fields': ['RequestedFor', 'IncidentDateTime', 'TypeOfIncident', 'IncidentLocation']
+                },
+                {
+                    'key': 'injury_information',
+                    'title': 'Injury Information',
+                    'fields': ['InjuryOccurence', 'AccidentCause', 'AccidentDescription']
+                },
+                {
+                    'key': 'additional_info',
+                    'title': 'Additional Information',
+                    'fields': ['PersonalProtectiveEquipment', 'WorkContinuation', 
+                               'AccidentWitness', 'FirstAidMeasures']
+                }
+            ]
+        }
+        
+        # Configure builder from the config dictionary
+        builder.configure_from_dict(config)
+        
+        # Return the built schema
+        return builder.build()
---
+++ backend/JsonSchemaForms/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class JsonschemaformsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'JsonSchemaForms'
