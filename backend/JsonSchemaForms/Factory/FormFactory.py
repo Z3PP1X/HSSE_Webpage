f307b6d490458deef58746a680a37629e0a1db40
Patrick <84373546+Z3PP1X@users.noreply.github.com>
2025-08-01 12:11:24 +0200

    JSON Schema Factory

---
+++ backend/JsonSchemaForms/Factory/FormFactory.py
@@ -0,0 +1,8 @@
+from abc import ABC, abstractmethod
+
+class FormFactory(ABC):
+    """Abstract FormFactory"""
+
+    @abstractmethod
+    def test():
+        pass
---
+++ backend/JsonSchemaForms/Factory/JSONForm.py
@@ -0,0 +1,9 @@
+from abc import ABC, abstractmethod
+
+
+class JSONForm(ABC):
+    """Abstract Form"""
+
+    @abstractmethod
+    def test():
+        pass
