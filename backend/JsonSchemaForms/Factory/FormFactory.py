9d269cf3386e44c6c8e88ee4fdf1cab96a11c2f9
Z3PP1X <p.fuchs@hm.edu>
2025-08-12 12:30:36 +0200

    Codebase cleanup

--- backend/JsonSchemaForms/Factory/FormFactory.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-class FormFactory(ABC):
-    """Abstract FormFactory"""
-
-    @abstractmethod
-    def test():
-        pass
--- backend/JsonSchemaForms/Factory/JSONForm.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class JSONForm(ABC):
-    """Abstract Form"""
-
-    @abstractmethod
-    def test():
-        pass
