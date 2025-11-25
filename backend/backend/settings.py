180122bb489297b3e5e8b9080166085eb9ecf430
Patrick <84373546+Z3PP1X@users.noreply.github.com>
2025-11-25 12:32:55 +0100

    removed dotenv

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -16,14 +16,6 @@ import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-env_file = find_dotenv()
-
-if not env_file:
-    raise FileNotFoundError("Keine .env-Datei gefunden!")
-
-load_dotenv(env_file)
-
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
