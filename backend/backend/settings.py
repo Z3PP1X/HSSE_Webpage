13bb8f7204469b6cd298fd117979a4d44d94d82c
Patrick <84373546+Z3PP1X@users.noreply.github.com>
2025-11-25 13:21:47 +0100

    fixes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'Core.middleware.currentUser.CurrentUserMiddleware',
+    #'Core.middleware.currentUser.CurrentUserMiddleware',
 
 
 
