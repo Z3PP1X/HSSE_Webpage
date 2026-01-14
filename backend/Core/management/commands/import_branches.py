07babf1cb18cfbddd72e4b1aaad4cf5e80cccd2b
Z3PP1X <p.fuchs@hm.edu>
2026-01-14 10:45:12 +0100

    ---

--- backend/Core/management/commands/import_branches.py
+++ backend/Core/management/commands/import_branches.py
@@ -23,30 +23,30 @@ class Command(BaseCommand):
 
                 reader = csv.DictReader(file, delimiter=';')
 
-            for entry in reader:
-                branch, created = BranchNetwork.objects.update_or_create(
-                    CostCenter=entry["Branch ID"],
-                    defaults={
+                for entry in reader:
+                    branch, created = BranchNetwork.objects.update_or_create(
+                        CostCenter=entry["Branch ID"],
+                        defaults={
 
-                            "BranchName": entry["Branch Name"],
-                            "RegionID": entry["Region ID"],
-                            "MandantID": entry["Mandant ID"],
-                            "RegionName": entry["Region Name"],
-                            "Country": entry["Country"],
-                            "Latitude": entry["Latitude"],
-                            "Longitude": entry["Longitude"],
-                            "State": entry["State"],
-                            "City": entry["City"],
-                            "Street": entry["Street"],
-                            "PostCode": entry["Post Code"],
-                            "BranchOperator": entry["Branch Operator"]
+                                "BranchName": entry["Branch Name"],
+                                "RegionID": entry["Region ID"],
+                                "MandantID": entry["Mandant ID"],
+                                "RegionName": entry["Region Name"],
+                                "Country": entry["Country"],
+                                "Latitude": entry["Latitude"],
+                                "Longitude": entry["Longitude"],
+                                "State": entry["State"],
+                                "City": entry["City"],
+                                "Street": entry["Street"],
+                                "PostCode": entry["Post Code"],
+                                "BranchOperator": entry["Branch Operator"]
 
 
-                        }
-                    )
-                action = "Created" if created else "Updated"
-                self.stdout.write(self.style.SUCCESS(
-                        f"{action}: {branch.BranchName}"))
+                            }
+                        )
+                    action = "Created" if created else "Updated"
+                    self.stdout.write(self.style.SUCCESS(
+                            f"{action}: {branch.BranchName}"))
 
         except FileNotFoundError:
             self.stderr.write(self.style.ERROR(f"Datei nicht gefunden: {file_path}"))
