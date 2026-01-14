e710aa6aa2efd282a469312ad6c9583c1380b4a7
Z3PP1X <p.fuchs@hm.edu>
2026-01-14 12:07:45 +0100

    changed alarmplan form config

--- backend/Alarmplan/config.py
+++ backend/Alarmplan/config.py
@@ -10,7 +10,7 @@ ALARMPLAN_FORM_CONFIG = {
                     "model": "Alarmplan",
                     "field": "RelatedBranch",
                     "ajax": {
-                        "endpoint": "/api/branchnetwork/costcenters/",
+                        "endpoint": "/api/branchnetwork/branchnetwork/",
                         "method": "GET",
                         "events": ["input", "focus"],
                         "debounce": 300,
